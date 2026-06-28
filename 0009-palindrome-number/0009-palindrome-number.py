7df449dc31f557a099a88bd2cc121ad1a1db4a2d
Kondakindha Vijay Kumar <162981898+comradevijay@users.noreply.github.com>
2026-06-28 19:20:20 +0530

    Time: 5 ms (71.48%), Space: 19.2 MB (85.89%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        rev = str(x)[::-1]
+        if str(x) == rev:
+            return True
+        else:
+            return False
+        
